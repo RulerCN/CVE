ce888f95b788f1273fd5aa9beec077cadd78d46d
Ruler <26105499@qq.com>
2018-04-08 18:18:16 +0800

    Initial commit

--- cve/CVE.cpp
+++ cve/CVE.cpp
@@ -8,6 +8,19 @@
 #include "core\cpu\convert_scale.h"
 #include "core\cpu\reduce.h"
 
+// Print vector
+template<class Allocator>
+void print(const char *name, const core::vector<signed char, Allocator> &vec);
+template<class Allocator>
+void print(const char *name, const core::vector<unsigned char, Allocator> &vec);
+template<class Allocator>
+void print(const char *name, const core::vector<signed short, Allocator> &vec);
+template<class Allocator>
+void print(const char *name, const core::vector<unsigned short, Allocator> &vec);
+template<class Allocator>
+void print(const char *name, const core::vector<int, Allocator> &vec);
+template<class Allocator>
+void print(const char *name, const core::vector<float, Allocator> &vec);
 // Print matrix
 template<class Allocator>
 void print(const char *name, const core::matrix<signed char, Allocator> &mat);
@@ -21,6 +34,7 @@ template<class Allocator>
 void print(const char *name, const core::matrix<int, Allocator> &mat);
 template<class Allocator>
 void print(const char *name, const core::matrix<float, Allocator> &mat);
+
 
 int main()
 {
@@ -36,8 +50,8 @@ int main()
 	core::vector<signed int> col_max(col, dim, static_cast<signed int>(core::int8_min));
 	core::vector<signed int> col_sum(col, dim, static_cast<signed int>(core::int32_zero));
 	//core::vector<signed int> row_min(row, dim, static_cast<signed int>(core::int8_max));
-	//core::vector<signed int> row_max(row, dim, static_cast<signed int>(core::int8_min));
-	//core::vector<signed int> row_sum(row, dim, static_cast<signed int>(core::int32_zero));
+	core::vector<signed int> row_max(row, dim, static_cast<signed int>(core::int8_min));
+	core::vector<signed int> row_sum(row, dim, static_cast<signed int>(core::int32_zero));
 	// Initialization matrix
 	x.linear_fill(static_cast<signed int>(1), static_cast<signed int>(2), static_cast<signed int>(1));
 	// Matrix operation
@@ -46,11 +60,14 @@ int main()
 	core::reduce(col_max, x, core::rm_col_max);
 	core::reduce(col_sum, x, core::rm_col_sum);
 	//core::reduce(row_min, x, core::rm_row_min);
-	//core::reduce(row_max, x, core::rm_row_max);
-	//core::reduce(row_sum, x, core::rm_row_sum);
+	core::reduce(row_max, x, core::rm_row_max);
+	core::reduce(row_sum, x, core::rm_row_sum);
+	print("X", x);
 	print("col_min", col_min);
 	print("col_max", col_max);
 	print("col_sum", col_sum);
+	print("row_max", row_max);
+	print("row_sum", row_sum);
 
 
 	//size_t row = 20;
@@ -65,6 +82,66 @@ int main()
 	//print("B", b);
 
     return 0;
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<signed char, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(static_cast<unsigned char>(*i)) << ",";
+	std::cout << "\n";
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<unsigned char, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(*i) << ",";
+	std::cout << "\n";
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<signed short, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << "0x" << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(static_cast<unsigned short>(*i)) << ",";
+	std::cout << "\n";
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<unsigned short, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << "0x" << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(*i) << ",";
+	std::cout << "\n";
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<signed int, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << "0x" << std::hex << std::setfill('0') << std::setw(8) << *i << ",";
+	std::cout << "\n";
+}
+
+template<class Allocator>
+void print(const char *name, const ::core::vector<float, Allocator> &vec)
+{
+	std::cout << name << "[" << vec.length() << "] =\n";
+	std::cout << "    ";
+	for (auto i = vec.begin(); i != vec.end(); ++i)
+		std::cout << std::setfill('0') << std::setw(8) << *i << ",";
+	std::cout << "\n";
 }
 
 template<class Allocator>
@@ -145,7 +222,7 @@ void print(const char *name, const core::matrix<float, Allocator> &mat)
 	{
 		std::cout << "    ";
 		for (auto i = mat.begin(j); i != mat.end(j); ++i)
-			std::cout << *i << ",";
+			std::cout << std::setfill('0') << std::setw(8) << *i << ",";
 		std::cout << "\n";
 	}
 	std::cout << "\n";
